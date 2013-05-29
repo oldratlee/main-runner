17b9357625b981398bd986ca155f696fa4a25a75
oldratlee <oldratlee@gmail.com>
2013-05-29 15:37:45 +0800

    add cygwin support

--- bin/env.sh
+++ bin/env.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 # determine base directory; preserve where you're running from
 
-CLASS_PATH=$BASE/../conf:`echo $BASE/../lib/*.jar | tr " " :`
 
+name=$(uname | tr A-Z a-z)
+
+case "${name}" in
+cygwin*)
+    for d in $BASE/../conf $(echo $BASE/../lib/*.jar) ; do
+        CLASS_PATH+="$(cygpath -w "${d}");"
+    done
+    ;;
+*)
+    CLASS_PATH=$BASE/../conf:`echo $BASE/../lib/*.jar | tr " " ;`
+    ;;
+esac 
